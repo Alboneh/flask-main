2b1758638f3f4260ff70b208b5503f7f75bad288
Alboneh <ibrahim.1208@students.amikom.ac.id>
2022-05-31 22:43:52 +0700

    add POST

--- api.py
+++ api.py
@@ -1,16 +1,20 @@
 import os
 from datetime import timedelta
-from flask import Flask,jsonify,request,make_response
+from flask import Flask,jsonify,request
 from util import Preprocessing
 from flask_jwt_extended import JWTManager,create_access_token,jwt_required,get_jwt_identity
 from database import init,getalluser,registerdb,logindb
 from flask_cors import CORS
 
 
-preprocess = Preprocessing("model.h5")
+#init flask and sql
 app = Flask(__name__)
 CORS(app)
 mysql = init(app)
+
+#load tensorflow model
+preprocess = Preprocessing("model.h5")
+prediction_results = preprocess.predict()
 
 #JWT
 app.config["JWT_SECRET_KEY"] = "capstone-secret-key" 
@@ -23,35 +27,86 @@ def index():
     #return jsonify(logged_in_as=current_user), 200
     return "Hello World"
 
-@app.route('/register', methods=['POST'])
-def register():
-    username = request.form['username']
-    email = request.form['email']
-    pwd = request.form['password']
-    return registerdb(mysql,username,email, pwd)
- 
-@app.route('/users',methods=['GET'])
-def userlist():
-    user = getalluser(mysql)
-    return jsonify(user)
-
-    
-@app.route('/predict', methods=['GET'])
-@jwt_required()
-def predict():
-  data = {'success': 'true','data': preprocess.predict()}                                                                                                                                                                                                                                                 
-  return jsonify(data)
-
 @app.route('/login', methods=['POST'])
 def login():
     email = request.form['email']
     pwd = request.form['password']
-    user = logindb(mysql,email,pwd)
-    if user != "":
-        access_token = create_access_token(identity=email)
-        data = {"message": "Login Successful" , "user": user, "access_token" : access_token}
-        return jsonify(data),200
-    return jsonify({"msg": "username atau password salah"}), 401
+    try:
+        user = logindb(mysql,email,pwd)
+        if user != "":
+            access_token = create_access_token(identity=email)
+            data = {"message": "Login Successful" , "user": user, "access_token" : access_token}
+            return jsonify(data),200
+        return jsonify({"msg": "username atau password salah"}), 401
+    except Exception as e:
+         err = jsonify(msg=f'{e}')
+         return err
+
+@app.route('/predict', methods=['GET'])
+@jwt_required()
+def predict():
+    try:
+        data = {'success': 'true','data': prediction_results}                                                                                                                                                                                                                                                 
+        return jsonify(data)
+    except Exception as e:
+         err = jsonify(msg=f'{e}')
+         return err
+
+@app.route("/predict/<product_name>", methods=['GET'])
+@jwt_required()
+def predictByProductName(product_name):
+    try:
+        try:
+            data = next(product for product in prediction_results if product["product_name"] == product_name)
+        except:
+            return jsonify(status_code=404, content = {"message": f"Product '{product_name}' doesn\'t exist"})
+        days = request.args.get('days', type = int)
+        if (days != None):
+            newdata = data["predictions"][:days]
+            return jsonify(predictions=newdata)
+        return data
+    except Exception as e:
+         err = jsonify(msg=f'{e}')
+         return err
+
+@app.route("/<product_name>", methods=['POST'])
+@jwt_required()
+def input_product(product_name):
+    try:
+        input_date = str(request.form['input_date'])
+        sold = float(request.form['sold'])
+        try:
+            data = next(product for product in prediction_results if product["product_name"] == product_name)
+        except:
+            return jsonify(status_code=404, content = {"message": f"Product '{product_name}' doesn\'t exist"})
+        
+        prediction = next(item for item in data["predictions"] if item["date"] == input_date)
+        prediction["real"] = sold
+        return data
+    except Exception as e:
+         err = jsonify(msg=f'{e}')
+         return err
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['name']
+    email = request.form['email']
+    pwd = request.form['password']
+    try:
+        return registerdb(mysql,username,email, pwd)
+    except Exception as e:
+         err = jsonify(msg=f'{e}')
+         return err
+ 
+@app.route('/users',methods=['GET'])
+def userlist():
+    try:
+        user = getalluser(mysql)
+        return jsonify(user)
+    except Exception as e:
+         err = jsonify(msg=f'{e}')
+         return err
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 80)))
--- util.py
+++ util.py
@@ -5,12 +5,13 @@ import tensorflow as tf
 from keras.preprocessing.sequence import TimeseriesGenerator
 
 class Preprocessing:
-    def __init__(self, model_path, data_name = "Groceries_dataset.csv", columns_to_be_removed = columns_to_be_removed):
-        self.columns_to_be_removed = columns_to_be_removed
+    def __init__(self, model_path, data_name = "Groceries_dataset.csv", columns_not_to_be_removed = columns_not_to_be_removed):
+        self.columns_not_to_be_removed = columns_not_to_be_removed
         self.data_name = data_name
         self.model = tf.keras.models.load_model(model_path)
         self.data_asarray = None
         self.columns = None
+        self.dates = None
 
     def sparse_data(self):
         raw_data = pd.read_csv(self.data_name)
@@ -22,11 +23,13 @@ class Preprocessing:
         sparse_data = pd.pivot_table(grouped_data, values = "count", columns = "itemDescription", index = "Date", aggfunc = np.sum)
         sparse_data = sparse_data.applymap(lambda x: 0 if np.isnan(x) else x)
 
-        for column in self.columns_to_be_removed:
-            if column in sparse_data.columns:
+        for column in sparse_data.columns:
+            if column not in columns_not_to_be_removed:
                 sparse_data.drop(column, axis = 1, inplace = True)
+
         self.data_asarray = sparse_data.values
         self.columns = sparse_data.columns
+        self.dates = [str(i) for i in sparse_data.index[-14:]]
 
     def predict(self):
         self.sparse_data()
@@ -41,8 +44,20 @@ class Preprocessing:
             X_last = X_last[1:]
 
         prediction_data = np.concatenate([X_new, X_last])
-        prediction_df = pd.DataFrame(prediction_data, columns=list(self.columns))
-        pred_dict = dict()
+        prediction_df = pd.DataFrame(prediction_data, columns=list(self.columns)).iloc[-14:, :]
+
+        pred_list = list()
         for column in prediction_df.columns:
-            pred_dict[column] = list(prediction_df[column])
-        return pred_dict
+            product_dict = dict()
+            product_dict["product_name"] = column
+            product_dict["predictions"] = list()
+            for date, value in zip(self.dates, prediction_df[column]):
+                date_and_value = dict()
+                date = date.replace("-", "/").split(" ")[0]
+                date_and_value["date"] = date
+                date_and_value["forecast"] = value
+                date_and_value["real"] = "-"
+                product_dict["predictions"].append(date_and_value)
+            pred_list.append(product_dict)
+        
+        return pred_list
