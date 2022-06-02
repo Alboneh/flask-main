e4f4f3cb8fd0af33a59f66e69ec4697a5923066b
daniel-bss <daniel.simamora2000@gmail.com>
2022-06-02 08:56:30 +0700

    update ML model and endpoint

---
+++ README.md
@@ -0,0 +1,1 @@
+# bangkit-capstone-project
---
+++ variables.py
@@ -0,0 +1,24 @@
+columns_not_to_be_removed = [
+    "beef",
+    "berries",
+    "brown bread",
+    "chicken",
+    "citrus fruit",
+    "condensed milk",
+    "fish",
+    "fruit/vegetable juice",
+    "grapes",
+    "ham",
+    "hamburger meat",
+    "herbs",
+    "honey",
+    "meat",
+    "onions",
+    "rice",
+    "spices",
+    "turkey",
+    "waffles",
+    "white bread",
+    "yogurt"
+]
+
