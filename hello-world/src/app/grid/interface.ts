acfcf3d63b40a5238bd51a0f333c9035323116c7
ibrahim <abahimyaim@gmail.com>
2023-06-08 03:06:50 +0700

    deleting hello-world

--- hello-world/src/app/grid/interface.ts
+++
@@ -1,9 +0,0 @@
-export interface interData {
-        id: number;
-        First_name: string;
-        Last_name: string;
-        Email: string;
-        Gender: string;
-        Address: string;
-        University: string;
-  }
